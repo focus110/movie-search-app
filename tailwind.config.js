2379b1f7fec0013e1f99f30b3fa4c1fff75da1e9
focus110 <iamemmaochuko@gmail.com>
2022-12-10 21:20:43 +0100

    updated readme

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "0rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+    },
+    extend: {
+      colors: {
+        primary: "#2B3B30",
+        "primary-dark": "#353935",
+        "btn-primary": "#353935",
+        "onyx-grey": "#F7F7F7",
+      },
+      backgroundImage: {
+        "hero-pattern": "url('/src/assets/images/hero-pattern.svg')",
+        "holding-phone": "url('/src/assets/images/holding-phone.png')",
+        "about-abstract": "url('/src/assets/images/abstract-bg.png')",
+      },
+    },
+  },
+  plugins: [],
+};
